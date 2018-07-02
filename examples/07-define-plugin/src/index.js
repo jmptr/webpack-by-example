d40cae3d71767ac9dbe89e1defed589b7d4c5526
JM Presley <jpresley@zulily.com>
2018-07-02 11:08:17 -0700

    add define plugin

---
+++ examples/07-define-plugin/src/index.js
@@ -0,0 +1,6 @@
+import image from './icons8-centralized-network-24.png';
+import css from './main.css';
+
+const environment = process.env.NODE_ENV;
+
+export { css, environment, image };
---
+++ examples/07-define-plugin/webpack.config.js
@@ -0,0 +1,49 @@
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+const extractTextLoader = ExtractTextPlugin.extract({
+  fallback: 'style-loader',
+  use: 'css-loader',
+});
+
+module.exports = {
+  mode: 'development',
+
+  devtool: 'source-map',
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+      },
+
+      {
+        test: /\.(jpe?g|png|gif)$/i,
+        loader: 'url-loader?limit=1000&name=/images/[hash].[ext]',
+      },
+
+      {
+        test: /\.(css)$/i,
+        use: extractTextLoader,
+      },
+
+    ],
+  },
+
+  plugins: [
+    new ExtractTextPlugin({ filename: '[name].css'}),
+    new CleanWebpackPlugin(['./dist']),
+    new HtmlWebpackPlugin({
+      title: 'webpack by example',
+      template: './src/index.html',
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
+      },
+    }),
+  ],
+};
