571656a3d64fd52877b12d33b7b7e089915239d8
JM Presley <jpresley@lastline.com>
2018-10-18 09:44:29 -0700

    add readme, update dependencies

---
+++ README.md
@@ -0,0 +1,6 @@
+# webpack-by-example
+
+webpack-by-example provides concrete examples of webpack configurations that demonstrate how webpack creates bundles of JavaScript code for the browser to run.
+
+# Example 01 Babel Loader
+
--- examples/01-babel-loader/webpack.config.js
+++ examples/01-babel-loader/webpack.config.js
@@ -1,11 +1,27 @@
 module.exports = {
+
   mode: 'development',
+
   devtool: 'source-map',
+
   module: {
     rules: [
       {
         test: /\.jsx?$/,
         loader: 'babel-loader',
+        exclude: /(node_modules|bower_components)/,
+        options: {
+          presets: [
+            [
+              '@babel/env',
+              { modules: false,  targets: { chrome: "52" } },
+            ],
+          ],
+          plugins: [
+            '@babel/proposal-class-properties',
+            '@babel/proposal-object-rest-spread',
+          ],
+        },
       },
     ],
   },
